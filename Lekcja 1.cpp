30703a1ad8faa6622049c020d0ca50424efdb08a
Serhii106 <sshcherbakov585@technikumkreatywne.pl>
2023-10-25 15:03:42 +0200

    Obnowa

--- Lekcja 1.cpp
+++ Lekcja 1.cpp
@@ -1,10 +1,39 @@
 #include <iostream> 
 using namespace std;
+
 int main(){
-	short zmienna = 2;
+	// deklaracja zmiennych 
+	short liczba = 2;
+	float zmiennoprzecinkowa = 3.21;
+	string tekst = "tekst";
+	char znak = 'A';
+	bool logiczny = false;
+	/*
       cout << "Hello world";
+      cout << endl; // koniec lini
+      cout << "Liczba: "  << liczba; // wyswietlenie zmiennej
       cout << endl;
-      cout << "Kolejny tekst";
-      cout << "Liczba: "  << zmienna;
+      */
+      /*
+      string wyraz;
+      cout << "Podaj jakis wyraz: ";
+      cin >> wyraz; 
+      cout << "Podales Wyraz: " << wyraz;
+      */
+      
+      string imie;
+      cout << "Podaj swoje imie: ";
+      cin >> imie;
+      cout << "Imie: " <<  imie;
+      cout << endl;
+      
+       short wiek;
+      cout << "Podaj wiek ";
+      cin >> wiek;
+      cout << "Podales wiek "<< wiek;
+      cout << endl;
+      
+      cout << " Witaj " << imie << " Masz " << wiek << " lat! " << endl;
+      
     return 0;
 }
